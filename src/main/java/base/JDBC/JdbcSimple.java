8cbc42b83e228d5e5267e2affc6f269b2d3c181e
wanglei <123456>
2017-11-05 00:05:18 +0800

    JDBC

--- src/main/java/base/JDBC/JdbcSimple.java
+++ src/main/java/base/JDBC/JdbcSimple.java
@@ -2,17 +2,36 @@ package base.JDBC;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * Created by wanglei on 2017/11/4.
  * 1:
  */
 public class JdbcSimple {
-    public static void main(String[] args) throws SQLException {
-        //1:
+    public static void main(String[] args)
+            throws SQLException
+    {
+        //1:注册数据库的驱动
         DriverManager.registerDriver(new com.mysql.jdbc.Driver());
-        Connection conn = DriverManager.getConnection("jdbc:mysql://192.168.126.142:3306/testJdbc");
+        //2:获取与数据库的连接
+        Connection conn = DriverManager.getConnection("jdbc:mysql://192.168.126.142:3306/testJdbc",
+                                                      "root",
+                                                      "hadoop");
+        //3:创建代表SQL语句的对象
+        Statement stmt = conn.createStatement();
+        //4:执行SQL语句
+        ResultSet resultSet = stmt.executeQuery("select distinct user_name from imooc_testJdbc");
+        //5:如果执行的是查询语句，要对结果进行处理
+        while(resultSet.next()){
+            String user_name = resultSet.getString("user_name");
+            System.out.println("user_name : "+user_name);
+        }
+        //6:释放占用的资源
+        stmt.close();
+        resultSet.close();
 
     }
 }
