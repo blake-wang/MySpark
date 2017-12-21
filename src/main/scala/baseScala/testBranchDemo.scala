c8c1ba6abde4eced5c02a5f8ea1aee5b9c808dc4
wanglei <wangl@yyft.com>
2017-12-21 13:44:20 +0800

    分支-wanglei

---
+++ src/main/scala/baseScala/testBranchDemo.scala
@@ -0,0 +1,12 @@
+package baseScala
+
+/**
+  * Created by bigdata on 17-12-21.
+  * 这个实在wanglei这个分支上创建的代码
+  */
+class testBranchDemo {
+  def main(args: Array[String]): Unit = {
+    println("hello git")
+  }
+
+}
