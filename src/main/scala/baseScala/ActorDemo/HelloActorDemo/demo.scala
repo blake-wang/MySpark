2eb65b16a3560a80f7ecd678478a04a5df3ae3e4
wanglei <362939327@qq.com>
2017-12-28 07:38:52 +0800

    类型参数

---
+++ src/main/scala/baseScala/ActorDemo/HelloActorDemo/demo.scala
@@ -0,0 +1,15 @@
+package baseScala.ActorDemo.HelloActorDemo
+
+/**
+  * Created by wanglei on 2017/12/27.
+  */
+object demo {
+  def main(args: Array[String]): Unit = {
+    val helloActor = new HelloActor
+    //启动actor
+    helloActor.start()
+    //向actor发送消息
+    helloActor!"leo"
+  }
+
+}
