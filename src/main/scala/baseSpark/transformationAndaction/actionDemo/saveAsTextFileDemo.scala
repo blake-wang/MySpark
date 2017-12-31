d4489b8c8d98a5b60f6d8155c298f8c8cc78dedd
wanglei <362939327@qq.com>
2017-12-31 15:31:08 +0800

    类型参数

---
+++ src/main/scala/baseSpark/transformationAndaction/actionDemo/saveAsTextFileDemo.scala
@@ -0,0 +1,8 @@
+package baseSpark.transformationAndaction.actionDemo
+
+/**
+  * Created by wanglei on 2017/12/31.
+  */
+object saveAsTextFileDemo {
+
+}
--- src/main/scala/baseSpark/transformationAndaction/actionDemo/takeDemo.scala
+++ src/main/scala/baseSpark/transformationAndaction/actionDemo/takeDemo.scala
@@ -4,18 +4,24 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 /**
   * Created by wanglei on 2017/12/31.
-  * count
   *
-  * 统计一个RDD中有几个元素
+  * take
+  *
+  * 获取集合中前几个元素
+  *
+  * take操作与collect类似，也是从远程集群上，获取rdd的数据
+  * 但是collect是获取rdd的所有数据，take只是获取前n个数据
+  *
   */
-object countDemo {
+object takeDemo {
+
   def main(args: Array[String]): Unit = {
     val sparkConf = new SparkConf().setAppName(this.getClass.getName.replace("$", "")).setMaster("local[1]")
     val sparkContext = new SparkContext(sparkConf)
-    val arr = Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val arr = Array(19, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
     val rdd = sparkContext.parallelize(arr)
-    //计算rdd中元素的数量
-    val num = rdd.count()
-    println(num)
+    val result = rdd.take(3)
+    println(result.toBuffer)
+    result.foreach(println(_))
   }
 }
