23f724451ee9328599e346e0220ae16117ad701c
wanglei <wangl@yyft.com>
2018-01-08 19:39:36 +0800

    row

--- src/main/java/base/object/CalendarDemo.java
+++ src/main/java/base/object/CalendarDemo.java
@@ -1,33 +1,63 @@
 package base.object;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by wanglei on 2018/1/5.
  * Calendar类  日历类
- *
+ * <p>
  * 成员方法
  * get()
  * set()
  */
 public class CalendarDemo {
     public static void main(String[] args) {
-        Calendar c = Calendar.getInstance();    //父类引用指向子类对象
+        //Calendar基本用法
+//        baseUse();
 
-        System.out.println(c.get(Calendar.YEAR));   //通过字段获取年
-        System.out.println(c.get(Calendar.MONTH));  //通过字段获取月，月是从0开始，0-11
-        System.out.println(c.get(Calendar.DAY_OF_MONTH));   //在当月的天数
-        System.out.println(c.get(Calendar.DAY_OF_YEAR));    //在当年的天数
-        System.out.println(c.get(Calendar.DAY_OF_WEEK));    //星期天是第一天，星期六是最后一天
+        //set and get
+//        setAndget();
 
-        System.out.println(c.get(Calendar.YEAR) + "年"
-                + getNum(c.get(Calendar.MONTH) + 1) + "月"
-                + getNum(c.get(Calendar.DAY_OF_MONTH)) + "日"
-                + getWeek(c.get(Calendar.DAY_OF_WEEK)));
-
-        System.out.println("--------------------------------------");
+        //setTime and getTime
+        setTimeAndgetTime();
 
 
+
+
+    }
+
+    /**
+     * 通过setTime和getTime来计算不同的天数以及不同的小时
+     */
+    private static void setTimeAndgetTime() {
+        Calendar c = Calendar.getInstance();
+        Date d = new Date();
+        c.setTime(d);
+        c.add(Calendar.DATE,-10);
+        Date time = c.getTime();
+        System.out.println(time);
+
+        SimpleDateFormat sip = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String format = sip.format(time);
+        System.out.println(format);
+
+        System.out.println("====================================");
+
+        Calendar c2 = Calendar.getInstance();
+        Date now = c2.getTime();
+        System.out.println("now : " + now);
+
+        Date d2 = new Date();
+        System.out.println("d2 : " +d2);
+        c2.setTime(d2);
+        System.out.println(c2);
+
+
+    }
+
+    private static void setAndget() {
         Calendar cc = Calendar.getInstance();
         System.out.println(cc.get(Calendar.YEAR) + "年"
                 + getNum(cc.get(Calendar.MONTH) + 1) + "月"
@@ -35,27 +65,68 @@ public class CalendarDemo {
                 + getWeek(cc.get(Calendar.DAY_OF_WEEK)));
 
         //add()   对指定的字段进行，向前件，向后加 计算日期
-        cc.add(Calendar.YEAR,1);
-        cc.add(Calendar.MONTH,0);
-        cc.add(Calendar.DAY_OF_MONTH,5);
+        cc.add(Calendar.YEAR, 1);
+        cc.add(Calendar.MONTH, 0);
+        cc.add(Calendar.DAY_OF_MONTH, 5);
         System.out.println(cc.get(Calendar.YEAR) + "年"
                 + getNum(cc.get(Calendar.MONTH) + 1) + "月"
                 + getNum(cc.get(Calendar.DAY_OF_MONTH)) + "日"
                 + getWeek(cc.get(Calendar.DAY_OF_WEEK)));
 
         //set()   根据指定字段，设置年，月，日
-        cc.set(Calendar.YEAR,2000);
+        cc.set(Calendar.YEAR, 2000);
 
         System.out.println(cc.get(Calendar.YEAR) + "年"
                 + getNum(cc.get(Calendar.MONTH) + 1) + "月"
                 + getNum(cc.get(Calendar.DAY_OF_MONTH)) + "日"
                 + getWeek(cc.get(Calendar.DAY_OF_WEEK)));
 
-        cc.set(2017,11,19);
+        cc.set(2017, 11, 19);
         System.out.println(cc.get(Calendar.YEAR) + "年"
                 + getNum(cc.get(Calendar.MONTH) + 1) + "月"
                 + getNum(cc.get(Calendar.DAY_OF_MONTH)) + "日"
                 + getWeek(cc.get(Calendar.DAY_OF_WEEK)));
+    }
+
+    private static void baseUse() {
+        Calendar c = Calendar.getInstance();    //父类引用指向子类对象
+        
+        int year = c.get(Calendar.YEAR);
+        System.out.println("year : "+year);
+
+        int month = c.get(Calendar.MONTH);
+        System.out.println("month : "+month);
+
+        int day = c.get(Calendar.DATE);
+        System.out.println("day : "+day);
+
+        int hour = c.get(Calendar.HOUR);
+        System.out.println("hour : "+hour);
+
+        int minute = c.get(Calendar.MINUTE);
+        System.out.println("minute : "+minute);
+
+        int second = c.get(Calendar.SECOND);
+        System.out.println("second : "+second);
+
+
+
+
+        System.out.println(c.get(Calendar.YEAR));           //通过字段获取年
+        System.out.println(c.get(Calendar.MONTH));          //通过字段获取月，月是从0开始，0-11
+        System.out.println(c.get(Calendar.DAY_OF_MONTH));   //在当月的天数
+        System.out.println(c.get(Calendar.DAY_OF_YEAR));    //在当年的天数
+        System.out.println(c.get(Calendar.DAY_OF_WEEK));    //星期天是第一天，星期六是最后一天
+        System.out.println(c.get(Calendar.DATE));           //获取日期，与DAY_OF_MONTH 相同
+        System.out.println(c.get(Calendar.HOUR));           //获取小时
+        System.out.println(c.get(Calendar.MINUTE));         //获取分钟数
+
+        System.out.println(c.get(Calendar.YEAR) + "年"
+                + getNum(c.get(Calendar.MONTH) + 1) + "月"
+                + getNum(c.get(Calendar.DAY_OF_MONTH)) + "日"
+                + getWeek(c.get(Calendar.DAY_OF_WEEK)));
+
+        System.out.println("--------------------------------------");
     }
 
     /**
---
+++ src/main/java/createData/createData.java
@@ -0,0 +1,11 @@
+package createData;
+
+/**
+ * Created by bigdata on 18-1-8.
+ * faker
+ */
+public class createData {
+    public static void main(String[] args) {
+
+    }
+}
