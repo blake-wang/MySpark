05f018913a5cc4eb89efa6bee55ac8ce1ac0624f
wanglei <wangl@yyft.com>
2018-01-08 22:33:07 +0800

    row

--- src/main/java/createData/createData.java
+++ src/main/java/createData/createData.java
@@ -1,12 +1,36 @@
 package createData;
 
+import com.github.javafaker.*;
+
+import java.util.Locale;
+
 /**
  * Created by bigdata on 18-1-8.
- * faker
+ * faker 造数据
  */
 public class createData {
     public static void main(String[] args) {
-        
+
+        Faker faker =new Faker(Locale.CHINA);
+
+        Internet internet = faker.internet();
+
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println(internet.url());
+
+        }
+
+        Book book = faker.book();
+        System.out.println(book.author());
+
+//        faker.s
+        Name name = faker.name();
+        System.out.println(name.name());
+        Address address = faker.address();
+        System.out.println(address.city());
+
+
 
     }
 }
