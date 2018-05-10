1e0fb1bcbdb384f31fd655d48cb7c7ca5da3f5df
wanglei <362939327@qq.com>
2018-05-10 08:00:11 +0800

    注解学习完成

---
+++ src/main/java/base/数据结构/ch02/BubbleSort.java
@@ -0,0 +1,18 @@
+package base.数据结构.ch02;
+
+public class BubbleSort {
+	
+	public static void sort(long[] arr) {
+		long tmp = 0;
+		for(int i = 0; i < arr.length - 1; i++) {
+			for(int j = arr.length - 1; j > i; j--) {
+				if(arr[j] < arr[j - 1]) {
+					//���н���
+					tmp = arr[j];
+					arr[j] = arr[j - 1];
+					arr[j - 1] = tmp;
+				}
+			}
+		}
+	}
+}
